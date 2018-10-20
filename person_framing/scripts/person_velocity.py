eab036d3a91eeaa855fb3351d16e496b2a9777c5
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2018-10-20 15:48:06 -0400

    Made person_velocity publish PoseStamped to make it visible in Rviz

--- person_framing/scripts/person_velocity.py
+++ person_framing/scripts/person_velocity.py
@@ -6,7 +6,7 @@ TODO: Implement network.
 """
 
 import rospy
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 import sys, select, termios, tty
 
 key_actions = {
@@ -17,6 +17,18 @@ key_actions = {
 }
 
 class PersonTracker(object):
+    @staticmethod
+    def pose_from_xy(x, y):
+        msg = PoseStamped(pose=Pose(position=Point(x, y, 0), orientation=Quaternion(0,0,0,0)))
+        msg.header.frame_id = 'map'
+        return msg
+
+    @staticmethod
+    def xy_from_pose(pose_stamped):
+        x = pose_stamped.pose.position.x
+        y = pose_stamped.pose.position.y
+        return (x, y)
+    
     def __init__(self):
         rospy.init_node('tracker_net')
 
@@ -24,17 +36,17 @@ class PersonTracker(object):
         self.settings = termios.tcgetattr(sys.stdin)
         self.movement_magnitude = 0.3
 
-        self.mock_point = Point(0,1,0)  # TODO: Once the neural net is implemented, stop publishing mock_point
+        self.mock_point = PersonTracker.pose_from_xy(0,1)  # TODO: Once the neural net is implemented, stop publishing mock_point
 
-        self.person_pub = rospy.Publisher("/person_position", Point, queue_size=10)
-
+        self.person_pub = rospy.Publisher("/person_position", PoseStamped, queue_size=10)
+        self.person_pub.publish(self.mock_point)
 
     def getKey(self):
         tty.setraw(sys.stdin.fileno())
         select.select([sys.stdin], [], [], 0)
         self.key_pressed = sys.stdin.read(1)
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-    
+
     def run(self):
         # Wait for a key to be pressed. Then, update the cmd_vel.
         while self.key_pressed != '\x03': # ctrl-C
@@ -55,14 +67,14 @@ class PersonTracker(object):
                 elif action == 'MOVE_BACKWARD':
                     y_diff = -1 * self.movement_magnitude
 
-                self.mock_point = Point(self.mock_point.x + x_diff, 
-                    self.mock_point.y + y_diff,
-                    self.mock_point.z)
+                x, y = PersonTracker.xy_from_pose(self.mock_point)
+
+                self.mock_point = PersonTracker.pose_from_xy(x + x_diff, y + y_diff)
 
                 self.person_pub.publish(self.mock_point)
 
             else:
-                print "Invalid key: {}".format(self.key_pressed)
+                print "Invalid key"
 
 if __name__ == "__main__":
     # Print out teleop options
