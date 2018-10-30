c9b05e42058f7ca2395cfefd95ce86a9a92915ef
cweiss <charlievweiss@gmail.com>
2018-10-30 12:15:57 -0400

    renamed files for better conventions and got launch file to accurately open person_pos

--- person_framing/scripts/person_pos.py
+++ person_framing/scripts/person_pos.py
@@ -20,7 +20,7 @@ class PersonTracker(object):
     @staticmethod
     def pose_from_xy(x, y):
         msg = PoseStamped(pose=Pose(position=Point(x, y, 0), orientation=Quaternion(0,0,0,0)))
-        msg.header.frame_id = 'map'
+        msg.header.frame_id = 'odom'
         return msg
 
     @staticmethod
