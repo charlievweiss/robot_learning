29f8767af40c027f96ad7c9ee16ca00b4adc296c
cweiss <charlievweiss@gmail.com>
2018-11-02 15:07:11 -0400

    loads img from neato and loads neural net

--- person_framing/scripts/person_pos.py
+++ person_framing/scripts/person_pos.py
@@ -2,12 +2,29 @@
 """
 Uses a neural network to determine the person's position on the screen and velocity.
 
-TODO: Implement network.
+TODO: 
+
+- Implement network. (Working on it -Charlie)
+- Apply same transforms to image as used for neural net
+- Obtain image from neato
+    CHECK SELF.PROCESS_IMAGE
+    CHECK GET_PREDICTED_POS
+
 """
 
 import rospy
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+from sensor_msgs.msg import LaserScan, Image
 import sys, select, termios, tty
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json, Model
+import numpy
+import os
+from cv_bridge import CvBridge
+import cv2
+import json
+import rospkg
 
 key_actions = {
     'a': 'MOVE_LEFT',
@@ -39,9 +56,38 @@ class PersonTracker(object):
 
         self.mock_point = PersonTracker.pose_from_xy(0,1)  # TODO: Once the neural net is implemented, stop publishing mock_point
 
+        # PUBLISHERS: publish point
         self.person_pub = rospy.Publisher("/person_position", PoseStamped, queue_size=10)
         self.person_pub.publish(self.mock_point)
 
+        # SUBSCSRIBER: get camera image
+        rospy.Subscriber('camera/image_raw', Image, self.get_image)
+        self.image = None
+        self.cv_bridge = CvBridge() # transfers ros images to opencv (python) images
+        # shows image
+        cv2.namedWindow('camera image')
+
+        # load model
+        # change model directory
+        r = rospkg.RosPack()
+        self.model_path = r.get_path('person_framing')
+        model_name = 'gnarlynet_errorfix'
+        json_file = open(self.model_path+'/models/{}.json'.format(model_name), 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        self.model = model_from_json(loaded_model_json)
+        # load weights into new model
+        self.model.load_weights(self.model_path+"/models/{}.h5".format(model_name))
+        print("Loaded model from disk")
+
+    def get_image(self, m):
+        im = self.cv_bridge.imgmsg_to_cv2(m, desired_encoding="bgr8")
+        self.image = im
+
+    """def get_predicted_pos(self):
+        predicted_pos = self.model.predict(image)"""
+
+    # for hypothetical point
     def getKey(self):
         tty.setraw(sys.stdin.fileno())
         select.select([sys.stdin], [], [], 0)
@@ -50,6 +96,15 @@ class PersonTracker(object):
 
     def run(self):
         # Wait for a key to be pressed. Then, update the cmd_vel.
+        while not rospy.is_shutdown():
+
+            """# Really scarily shows a bunch of images, but hey, we get images.
+            cv2.imshow("camera image",self.image)
+            key = cv2.waitKey(100)
+            cv2.destroyAllWindows()"""
+            
+
+        """
         while self.key_pressed != '\x03': # ctrl-C
             self.getKey()
 
@@ -76,12 +131,13 @@ class PersonTracker(object):
 
             else:
                 print "Invalid key"
+        """
 
 if __name__ == "__main__":
-    # Print out teleop options
+    """# Print out teleop options
     print "Use the following keys to move the Neato around:"
     for k in key_actions.keys():
-        print "'{}' : \t{}".format(k, key_actions[k])
+        print "'{}' : \t{}".format(k, key_actions[k])"""
 
     node = PersonTracker()
     node.run()
